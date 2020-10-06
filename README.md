7d54d0ed91a92c91c57d40775b19508f6e68fd73
DESKTOP-8JNCV7Q\dell <danieldada123@gmail.com>
2020-10-06 15:38:15 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# loadbalancerdemo-discovery-service" 
---
+++ src/main/java/com/example/discoveryeurekaservice/DiscoveryEurekaServiceApplication.java
@@ -0,0 +1,15 @@
+package com.example.discoveryeurekaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryEurekaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryEurekaServiceApplication.class, args);
+	}
+
+}
